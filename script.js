16eb887751677480debbbe61e2a98283d46c8cb6
Stacey Csikos <stacey@Staceys-MacBook-Pro.local>
2022-03-10 13:33:46 -0500

    MVP

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
-//https://disneyapi.dev
-
-
-let list = document.querySelector(".some-stuff");
 let mario = document.querySelector(".first-pic");
 let peach = document.querySelector(".second-pic");
 let bowser = document.querySelector(".third-pic");
+let marioBtn = document.querySelector(".mario-button");
+let peachBtn = document.querySelector(".peach-button");
+let bowserBtn = document.querySelector(".bowser-button");
+let marioSpeach = document.querySelector(".mario-shines");
+let exitMario = document.querySelector(".mario-close");
+let peachSpeach = document.querySelector(".peach-shines");
+let exitPeach = document.querySelector(".peach-close");
+let bowserSpeach = document.querySelector(".bowser-shines");
+let exitBowser = document.querySelector(".bowser-close");
+let page= document.querySelector(".container")
 
 //gets pictures for the mario, peach, bowser
 fetch("https://www.amiiboapi.com/api/amiibo/?gameseries=0x00")
   .then((response) => response.json())
   .then((data) => {
-    console.log(data.amiibo);
-    // let string = "";
-    for (let i = 0; i < data.amiibo.length; i++) {
-      // string += `<li>${data.amiibo[i].character}</li>`
-      // console.log(data.amiibo[i].character, i, data.amiibo[i].image)
-      // console.log(data.amiibo[i].gameseries, i)
-      // list.innerHTML = string
-    }
+    mario.src = data.amiibo[6].image;
+    peach.src = data.amiibo[15].image;
+    bowser.src = data.amiibo[29].image;
+  });
 
-    console.log(data.amiibo[6].image)
-    mario.src = data.amiibo[6].image 
-    peach.src = data.amiibo[15].image
-    bowser.src = data.amiibo[29].image
+marioBtn.addEventListener("click", () => {
+  marioSpeach.style.display = "block";
+  exitMario.addEventListener("click", () => {
+    marioSpeach.style.display = "none";
+  });
+});
 
+peachBtn.addEventListener("click", () => {
+  peachSpeach.style.display = "block";
+  pagePeach.addEventListener("click", () => {
+    peachSpeach.style.display = "none";
+  });
+});
 
-  })
-
-//pictures for the food
-fetch("https://foodish-api.herokuapp.com/api/")
-
-
+bowserBtn.addEventListener("click", () => {
+  bowserSpeach.style.display = "block";
+  exitBowser.addEventListener("click", () => {
+    bowserSpeach.style.display = "none";
+  });
+});
 
